4a3cdd3229d253e9f29f99fae080b68a5bbab9a3
FekalisPoganchik <igorlebedev018@gmail.com>
2023-09-10 16:32:20 +0500

    Task1

---
+++ Tyuiu.LebedevIV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.LebedevIV.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Игорь";
+            int age = 21;
+            Console.WriteLine($"Здравствуйте, меня зову {name} мне {age} год");
+            Console.ReadKey();
+        }
+    }
+}
